b431325be3807dd3f54d8a41355ad0cce66e96ae
Shrey Baxi <baxishrey@gmail.com>
2018-10-29 22:18:13 +0530

    Added scanner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'shrey-qr-scanner';
+export class AppComponent implements OnInit {
+
+  @ViewChild('scanner') scanner: ZXingScannerComponent;
+
+  ngOnInit(): void {
+    this.scanner.camerasFound.subscribe(res => console.log(res));
+  }
 }
